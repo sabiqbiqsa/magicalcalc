70a731120fadd1e53beeadb559d7876c5b416260
sabiqbiqsa <sabiqanonyma@gmail.com>
2020-04-17 14:58:59 -0300

    final version of the code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+<<<<<<< Updated upstream
 print("our magical calculator")
 run=True
 remain=0
@@ -11,6 +12,25 @@ while run:
     else:
         while check!="q":
             remain += eval(check)
+=======
+print("Our magical calculator")
+print("Type 'q' and press enter to quit")
+run = True
+remain = ""
+while run:
+    check = str(input("Type your equation"))
+    check = re.sub('[A-Za-pr-z,.?><#@!()]', '', check)
+    if check == "q":
+        run = False
+        print("Thanks for using our app")
+    else:
+        while check != "q":
+            try:
+                equation = eval(remain+check)
+                remain = str(equation)
+            except SyntaxError:
+                print("Please type a real equation")
+>>>>>>> Stashed changes
             check = str(input(remain))
             check = re.sub('[A-Za-pr-z,.?/><#@!()]', '', check)
         run = False
