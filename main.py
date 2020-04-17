c6a1218e86c1c9cccb1b661fad6fd3f43c004b3e
sabiqbiqsa <sabiqanonyma@gmail.com>
2020-04-17 00:20:29 -0300

    it's perfect now , i used the try\except to solve the error

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import re
+
 print("our magical calculator")
-run=True
-remain=0
+run = True
+remain = 0
 while run:
     check = str(input("type your equation"))
-    check= re.sub('[A-Za-pr-z,.?/><#@!()]','',check)
-    if check=="q":
-        run=False
+    check = re.sub('[A-Za-pr-z,.?/><#@!()]', '', check)
+    if check == "q":
+        run = False
         print("Thanks for using our app")
     else:
-        while check!="q":
-            remain += eval(check)
+        while check != "q":
+            try:
+                remain += eval(check)
+            except:
+                print("You are stupid")
             check = str(input(remain))
             check = re.sub('[A-Za-pr-z,.?/><#@!()]', '', check)
         run = False
