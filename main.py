492b5b66dffa464e23ad51910ab06ebd5702ca94
sabiqbiqsa <sabiqanonyma@gmail.com>
2020-04-17 14:30:50 -0300

    Finally resolved the equation
    i was facing and issue while the user wasn't ablee to make a cumule of the eqution, for instance when the user pressed multiply or divided while a previous equation was made it crashed but now it's okay i properly used the eval fonction by storing previous operation in a variable and then using it with the eval fonction and the next operation

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import re
-
-print("our magical calculator")
+print("Our magical calculator")
+print("Type 'q' and press enter to quit")
 run = True
-remain = 0
+remain = ""
+equation = 0
 while run:
     check = str(input("type your equation"))
-    check = re.sub('[A-Za-pr-z,.?/><#@!()]', '', check)
+    check = re.sub('[A-Za-pr-z,.?><#@!()]', '', check)
     if check == "q":
         run = False
         print("Thanks for using our app")
     else:
         while check != "q":
             try:
-                remain += eval(check)
-            except:
-                print("You are stupid")
+                equation = eval(remain+check)
+                remain = str(equation)
+            except SyntaxError:
+                print("Please check your equation")
             check = str(input(remain))
-            check = re.sub('[A-Za-pr-z,.?/><#@!()]', '', check)
+            check = re.sub('[A-Za-pr-z,.?><#@!()]', '', check)
         run = False
         print("Thanks for using our app")
